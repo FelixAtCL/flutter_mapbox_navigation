8445b220bcbd26e8699fe452a067c8c38bec3bbf
Felix <felix@coding-lane.com>
2024-03-29 15:05:20 +0100

    added add and remove listener to gestures api

--- lib/src/embedded/boundings/map/gestures/port/api.dart
+++ lib/src/embedded/boundings/map/gestures/port/api.dart
@@ -1,5 +1,7 @@
 part of '../../../../../../mapbox_navigation_flutter.dart';
 
+typedef MapGestureListener = void Function(ScreenCoordinate coordinate);
+
 /// Interface for managing gestures of the `map`.
 class GestureAPI {
   /// Constructor for [GestureAPI].
@@ -9,13 +11,37 @@ class GestureAPI {
       const StandardMethodCodec(GestureListenerCodec()),
     );
     _methodChannel.setMethodCallHandler(_handleMethod);
+  }
 
-    _eventChannel =
-        EventChannel('flutter_mapbox_navigation/gestures/$id/events');
+  void addListenerOnMapTap(MapGestureListener listener) {
+    _onTapMap = listener;
+  }
+
+  void removeListenerOnMapTap() {
+    _onTapMap = null;
+  }
+
+  void addListenerOnScrollMap(MapGestureListener listener) {
+    _onScrollMap = listener;
+  }
+
+  void removeListenerOnScrollMap() {
+    _onScrollMap = null;
+  }
+
+  void addListenerOnLongTapMap(MapGestureListener listener) {
+    _onLongTapMap = listener;
+  }
+
+  void removeListenerOnLongTapMap() {
+    _onLongTapMap = null;
   }
 
   late MethodChannel _methodChannel;
-  late EventChannel _eventChannel;
+
+  MapGestureListener? _onTapMap;
+  MapGestureListener? _onScrollMap;
+  MapGestureListener? _onLongTapMap;
 
   /// Generic Handler for Messages sent from the Platform
   Future<dynamic> _handleMethod(MethodCall call) async {
@@ -24,13 +50,16 @@ class GestureAPI {
         final text = call.arguments as String?;
         return Future.value('Text from native: $text');
       case 'onMapTap':
-        print("onMapTap called with ${call.arguments}");
+        if (call.arguments is! ScreenCoordinate) break;
+        _onTapMap?.call(call.arguments as ScreenCoordinate);
         break;
       case 'onScrollMap':
-        print("onScrollMap called with ${call.arguments}");
+        if (call.arguments is! ScreenCoordinate) break;
+        _onScrollMap?.call(call.arguments as ScreenCoordinate);
         break;
       case 'onLongTapMap':
-        print("onLongTapMap called with ${call.arguments}");
+        if (call.arguments is! ScreenCoordinate) break;
+        _onLongTapMap?.call(call.arguments as ScreenCoordinate);
         break;
       default:
         print("${call.method}: ${call.arguments}");
