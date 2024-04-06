9d1a88806ce13fd8c35d3cf95e68d6b91c7b2abc
Felix <felix@coding-lane.com>
2024-04-06 11:32:44 +0200

    update in turf adapter

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/core/TurfAdapter.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/core/TurfAdapter.kt
@@ -3,8 +3,8 @@ package com.eopeter.fluttermapboxnavigation.core
 import com.google.gson.Gson
 import com.mapbox.geojson.Point
 
-fun Point.toList(): List<Any?> {
-    return listOf(mapOf("coordinates" to coordinates()))
+fun Point.toList(): Any? {
+    return mapOf("coordinates" to coordinates())
 }
 
 object PointDecoder {
