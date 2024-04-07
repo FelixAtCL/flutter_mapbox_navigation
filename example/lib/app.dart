f1b30324e2f2447f4f944da9158c381bf63358b4
Felix <felix@coding-lane.com>
2024-04-07 13:13:12 +0200

    error in event listener android

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -279,6 +279,7 @@ class _SampleNavigationAppState extends State<SampleNavigationApp> {
     });
   }
 
+  // TODO: Android error on this part -> Maybe in the decoder parts?!
   _listenOnCameraChange() {
     _controller?.map.addEventListener((event) {
       print("camera changed: ${event.data}");
