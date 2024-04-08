efcc89e13d74fbc8ccb279461944e2061a273950
Felix <felix@coding-lane.com>
2024-04-08 20:41:11 +0200

    try try try

--- lib/src/embedded/boundings/map/port/api.dart
+++ lib/src/embedded/boundings/map/port/api.dart
@@ -103,7 +103,7 @@ class MapAPI {
     for (final element in events) {
       final args = <String, dynamic>{};
       args['event'] = element.name;
-      await _methodChannel.invokeMethod('listenOnEvent', args);
+      final result = await _methodChannel.invokeMethod('listenOnEvent', args);
       if (_observers[element.name] == null) {
         // Haven't subscribed this event
         _observers[element.name] = [observer];
