b4e7ff9f8df1fd059c7d345879ba95af1fee6763
Felix <felix@coding-lane.com>
2024-04-18 22:21:04 +0200

    fix

--- lib/src/models/helpers/route_options.dart
+++ lib/src/models/helpers/route_options.dart
@@ -18,8 +18,8 @@ class RouteOptions {
 
   static RouteOptions? fromAndroid(Map<String, dynamic>? json) => json != null
       ? RouteOptions._(
-          arriveBy: DateTime.tryParse(json['arriveBy'] as String),
-          departAt: DateTime.tryParse(json['departAt'] as String),
+          arriveBy: DateTime.tryParse(json['arriveBy'] as String? ?? ''),
+          departAt: DateTime.tryParse(json['departAt'] as String? ?? ''),
           maximumHeight: double.parse(json['maxHeight'].toString()),
           maximumWidth: double.parse(json['maxWidth'].toString()),
           maximumWeight: double.parse(json['maxWeight'].toString()),
@@ -28,8 +28,8 @@ class RouteOptions {
 
   static RouteOptions? fromIOS(Map<String, dynamic>? json) => json != null
       ? RouteOptions._(
-          arriveBy: DateTime.tryParse(json['arriveBy'] as String),
-          departAt: DateTime.tryParse(json['departAt'] as String),
+          arriveBy: DateTime.tryParse(json['arriveBy'] as String? ?? ''),
+          departAt: DateTime.tryParse(json['departAt'] as String? ?? ''),
           maximumHeight: double.parse(json['maximumHeight'].toString()),
           maximumWidth: double.parse(json['maximumWidth'].toString()),
           maximumWeight: double.parse(json['maximumWeight'].toString()),
